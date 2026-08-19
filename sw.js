e5bbb478e83c8df7992aedd4fb6973a30f6d8fbe
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-19 02:28:27 +0100

    Forçar atualização do módulo Vida pessoal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='foco-jornada-v4-2-0-couple';
+const CACHE='foco-jornada-v4-2-0-couple2';
 const ASSETS=['./','./index.html','./styles.css','./ux.css','./features.css','./hub.css','./couple.css','./core.js','./app.js','./stability.js','./ux.js','./features-core.js','./features.js','./hub.js','./controls.js','./couple-core.js','./couple.js','./manifest.webmanifest','./icon.svg','./.nojekyll'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
