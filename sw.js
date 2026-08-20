df07888acfe7b87eee8487413f4ba036e2ae5538
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-20 18:34:10 +0100

    Renovar cache final da interface linear

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='foco-jornada-v4-2-0-linear-planning1';
+const CACHE='foco-jornada-v4-2-0-linear-planning2';
 const ASSETS=[
   './','./index.html',
   './styles.css','./ux.css','./features.css','./hub.css','./hub-about.css','./settings-controller.css','./linear-ui.css',
