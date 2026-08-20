5d023366c5a0cea1ba94c6186d219192f8d2901e
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-20 20:39:03 +0100

    feat: criar pagina Mais

---
+++ src/presentation/pages/MorePage.tsx
@@ -0,0 +1,11 @@
+import { PagePlaceholder } from '../components/PagePlaceholder'
+
+export function MorePage() {
+  return (
+    <PagePlaceholder
+      eyebrow="MAIS"
+      title="Mais"
+      description="Estatísticas, exportações e acessos secundários serão concentrados nesta área."
+    />
+  )
+}
