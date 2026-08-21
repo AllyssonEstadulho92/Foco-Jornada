a56a8c63ce2e12e71447352f0b609b499c7572b3
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-21 13:30:20 +0100

    Guia: adicionar rota de utilização

--- src/presentation/router.tsx
+++ src/presentation/router.tsx
@@ -2,6 +2,7 @@ import { createHashRouter } from 'react-router-dom'
 import { AppShell } from './layouts/AppShell'
 import { ActivitiesPage } from './pages/ActivitiesPage'
 import { FocusPage } from './pages/FocusPage'
+import { GuidePage } from './pages/GuidePage'
 import { HistoryPage } from './pages/HistoryPage'
 import { MorePage } from './pages/MorePage'
 import { PayrollPage } from './pages/PayrollPage'
@@ -20,6 +21,7 @@ export const router = createHashRouter([
       { path: 'foco', element: <FocusPage /> },
       { path: 'historico', element: <HistoryPage /> },
       { path: 'mais', element: <MorePage /> },
+      { path: 'guia', element: <GuidePage /> },
       { path: 'vencimento', element: <PayrollPage /> },
       { path: 'horas', element: <WorkHoursCalculatorPage /> },
       { path: 'estatisticas', element: <StatisticsPage /> },
