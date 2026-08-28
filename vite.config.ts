2e5eb323a82815924a421b50e8b37b944a0c3bde
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-29 01:21:47 +0200

    pwa: incluir ilustração webp no precache

--- vite.config.ts
+++ vite.config.ts
@@ -60,7 +60,7 @@ export default defineConfig({
       workbox: {
         navigateFallback: 'index.html',
         cleanupOutdatedCaches: true,
-        globPatterns: ['**/*.{js,css,html,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,html,svg,webp,woff2}'],
       },
     }),
   ],
