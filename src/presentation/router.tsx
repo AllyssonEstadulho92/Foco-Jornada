1a1059de95711f0d186a3b384c7a03d287569c1f
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-31 22:48:30 +0200

    feat: ligar dashboard, calendário, notificações e relatórios v2

--- src/presentation/router.tsx
+++ src/presentation/router.tsx
@@ -8,14 +8,17 @@ import { HistoryPage } from './pages/HistoryPage'
 import { MedicationActionsGuidePage } from './pages/MedicationActionsGuidePage'
 import { MedicationsStockPage } from './pages/MedicationsStockPage'
 import { MoreWithBackupPage } from './pages/MoreWithBackupPage'
+import { NotificationCenterV2Page } from './pages/NotificationCenterV2Page'
+import { OperationalCalendarPage } from './pages/OperationalCalendarPage'
 import { PayrollPage } from './pages/PayrollPage'
 import { PayrollReferencePage } from './pages/PayrollReferencePage'
 import { PersonalStockHubPage } from './pages/PersonalStockHubPage'
+import { ReportsV2Page } from './pages/ReportsV2Page'
 import { SettingsReferencePage } from './pages/SettingsReferencePage'
 import { ShiftMapPage } from './pages/ShiftMapPage'
 import { StatisticsPage } from './pages/StatisticsPage'
 import { SticksStockPage } from './pages/SticksStockPage'
-import { TodayReferencePage } from './pages/TodayReferencePage'
+import { TodayV2Page } from './pages/TodayV2Page'
 import { WorkHoursCalculatorPage } from './pages/WorkHoursCalculatorPage'
 
 export const router = createHashRouter([
@@ -23,7 +26,10 @@ export const router = createHashRouter([
     path: '/',
     element: <AppShell />,
     children: [
-      { index: true, element: <TodayReferencePage /> },
+      { index: true, element: <TodayV2Page /> },
+      { path: 'calendario', element: <OperationalCalendarPage /> },
+      { path: 'notificacoes', element: <NotificationCenterV2Page /> },
+      { path: 'relatorios', element: <ReportsV2Page /> },
       { path: 'turnos', element: <ShiftMapPage /> },
       { path: 'atividades', element: <ActivitiesPage /> },
       { path: 'foco', element: <FocusPage /> },
