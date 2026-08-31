a48f218db0a7b770876734ef9c0c08acd29e019d
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-01 01:05:34 +0200

    Usar novo Início de referência

--- src/presentation/router.tsx
+++ src/presentation/router.tsx
@@ -5,6 +5,7 @@ import { ExportDataPage } from './pages/ExportDataPage'
 import { FocusPage } from './pages/FocusPage'
 import { GuideWithMedicationPage } from './pages/GuideWithMedicationPage'
 import { HistoryPage } from './pages/HistoryPage'
+import { HomeReferencePage } from './pages/HomeReferencePage'
 import { MedicationActionsGuidePage } from './pages/MedicationActionsGuidePage'
 import { MedicationsStockPage } from './pages/MedicationsStockPage'
 import { MoreWithBackupPage } from './pages/MoreWithBackupPage'
@@ -18,7 +19,6 @@ import { SettingsReferencePage } from './pages/SettingsReferencePage'
 import { ShiftMapPage } from './pages/ShiftMapPage'
 import { StatisticsPage } from './pages/StatisticsPage'
 import { SticksStockPage } from './pages/SticksStockPage'
-import { TodayReferencePage } from './pages/TodayReferencePage'
 import { WorkHoursCalculatorPage } from './pages/WorkHoursCalculatorPage'
 
 export const router = createHashRouter([
@@ -26,7 +26,7 @@ export const router = createHashRouter([
     path: '/',
     element: <AppShell />,
     children: [
-      { index: true, element: <TodayReferencePage /> },
+      { index: true, element: <HomeReferencePage /> },
       { path: 'calendario', element: <OperationalCalendarPage /> },
       { path: 'notificacoes', element: <NotificationCenterV2Page /> },
       { path: 'relatorios', element: <ReportsV2Page /> },
