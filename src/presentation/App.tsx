fb90e564a4e3e04d23690718b001c594b117ba70
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-10 07:46:29 +0100

    feat: ativar automação global do horário configurado

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
+import { ScheduledWorkdayAutomation } from './components/ScheduledWorkdayAutomation'
 import { AppServicesProvider, type AppServices } from './providers/AppServicesProvider'
 import { router } from './router'
 
 export function App({ services }: { services: AppServices }) {
   return (
     <AppServicesProvider services={services}>
+      <ScheduledWorkdayAutomation />
       <RouterProvider router={router} />
       <Toaster position="top-right" toastOptions={{ duration: 3500 }} />
     </AppServicesProvider>
